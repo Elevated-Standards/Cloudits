b7018a4d0175f71b86db997ae40f306da68f25c2
Austin Songer, CISSP <a.songer@protonmail.com>
2025-01-17 14:41:51 -0600

    Update cloudprefix.services.py

--- src/services/aws/cloudprefix.services.py
+++ src/services/aws/cloudprefix.services.py
@@ -8,6 +8,12 @@ from credentials.aws import get_aws_credentials
 # Ensure the 'src' directory is in the Python module search path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
+# Define toggles to enable or disable environments
+enable_environments = {
+    'commercial': True,  # Set to False to disable 'commercial'
+    'federal': False      # Set to False to disable 'federal'
+}
+
 YEAR = datetime.datetime.now().year
 MONTH = datetime.datetime.now().strftime('%B')
 DAY = datetime.datetime.now().day
