4501d8acfc3db6b9ecd21ca6fdf10eb5f355b026
Austin Songer, CISSP <a.songer@protonmail.com>
2025-01-17 22:38:24 +0000

    Update output file paths to include month in AWS environment configurations

--- src/services/aws/disaster.services.py
+++ src/services/aws/disaster.services.py
@@ -30,19 +30,19 @@ environments = {
     'commercial': {
         'region': 'us-east-1',
         'output_files': {
-            'backup_vaults': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}-backup_vaults.json",
-            'backup_plans': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}-backup_plans.json",
-            'recovery_points': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}-recovery_points.json",
-            'tags': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}-backup_tags.json"
+            'backup_vaults': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}/{END_DATE}-backup_vaults.json",
+            'backup_plans': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}/{END_DATE}-backup_plans.json",
+            'recovery_points': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}/{END_DATE}-recovery_points.json",
+            'tags': f"{BASE_DIR}/commercial/systems/aws/{YEAR}/{MONTH}/{END_DATE}-backup_tags.json"
         }
     },
     'federal': {
         'region': 'us-east-1',
         'output_files': {
-            'backup_vaults': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}-backup_vaults.json",
-            'backup_plans': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}-backup_plans.json",
-            'recovery_points': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}-recovery_points.json",
-            'tags': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}-backup_tags.json"
+            'backup_vaults': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}/{END_DATE}-backup_vaults.json",
+            'backup_plans': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}/{END_DATE}-backup_plans.json",
+            'recovery_points': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}/{END_DATE}-recovery_points.json",
+            'tags': f"{BASE_DIR}/federal/systems/aws/{YEAR}/{MONTH}/{END_DATE}-backup_tags.json"
         }
     }
 }
