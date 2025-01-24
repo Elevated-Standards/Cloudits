636f216f45553d9ea0da3d66013ddaf165d48a6b
Austin Songer, CISSP <a.songer@protonmail.com>
2025-01-24 03:06:38 +0000

    Refactor pyproject.toml and setup.py to streamline dependencies and project structure

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="Cloudits",
+    package_dir={"": "src"},
     version="0.1.0",
     description="Your project description",
+    use_incremental=True,
     packages=find_packages(),
     install_requires=[
         # Standard dependencies
-        "some-other-library",
-
-        # Placeholder for manual requirements installation
+        "incremental",
         # Incremental must be installed separately from requirements.txt
     ],
 )
