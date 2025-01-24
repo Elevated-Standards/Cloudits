5e6219e9321d008a265fba350fed892492da94df
Austin Songer, CISSP <a.songer@protonmail.com>
2025-01-23 20:38:44 -0600

    Create _version.py

---
+++ src/services/cloudits/_version.py
@@ -0,0 +1,9 @@
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update example_setuptools` to change this file.
+
+from incremental import Version
+
+__version__ = Version("cloudits", 2, 3, 4)
+
+__all__ = ["__version__"]
+
