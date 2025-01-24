0bae6e200cffb4608a3f3f47fa04ba1b2d1a5fd4
Austin Songer, CISSP <a.songer@protonmail.com>
2025-01-23 20:34:28 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Cloudits",
+    version="0.1.0",
+    description="Your project description",
+    packages=find_packages(),
+    install_requires=[
+        # Standard dependencies
+        "some-other-library",
+
+        # Placeholder for manual requirements installation
+        # Incremental must be installed separately from requirements.txt
+    ],
+)
+
